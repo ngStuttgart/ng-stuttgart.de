c8167417b63f62069a7c411ce8ca4366bbf7677d
DrMabuse <drmabuse@posteo.de>
2019-01-20 09:28:44 +0100

    feat: add swagger documentation

--- apps/nestjs-app/src/main.ts
+++ apps/nestjs-app/src/main.ts
@@ -6,9 +6,18 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Swagger Ng Stuttgart')
+    .setDescription('The API')
+    .setVersion('1.0')
+    .addTag('meetup')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3333, () => {
     console.log('Listening at http://localhost:3333');
   });
