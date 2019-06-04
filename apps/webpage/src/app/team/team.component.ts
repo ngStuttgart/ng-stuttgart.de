9f3771af6f0a6c0a672777d1e5cbbc67283389bb
Alexander Schuster <alexander.schuster@web.de>
2019-06-04 19:30:34 +0200

    feat: fix home

--- apps/webpage/src/app/team/team.component.ts
+++ apps/webpage/src/app/team/team.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 interface TeamMember {
   name: string;
-  twitter: string;
-  image?: string;
+  twitterHandle: string;
+  imagePath?: string;
 }
 
 @Component({
@@ -12,38 +12,36 @@ interface TeamMember {
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent {
   teamMembers$: Observable<TeamMember[]>;
 
   constructor() {
     this.teamMembers$ = of([
       {
         name: 'Florian Tischler',
-        twitter: '@floriantischler',
-        image: 'assets/profiles/25cf5a1c29a3d81d09b3c29a8ffad66b.png'
+        twitterHandle: '@floriantischler',
+        imagePath: 'assets/profiles/25cf5a1c29a3d81d09b3c29a8ffad66b.png'
       },
       {
         name: 'Marco Hämmerle',
-        twitter: '@marcohaemmerle',
-        image: 'assets/profiles/197b3c0a7032750d5b1769c75d45f71b.png'
+        twitterHandle: '@marcohaemmerle',
+        imagePath: 'assets/profiles/197b3c0a7032750d5b1769c75d45f71b.png'
       },
       {
         name: 'Pascal Brewing',
-        twitter: '@DocMaboos',
-        image: 'assets/profiles/eaabfc300beb14a1257d6ba9326bb09a.png'
+        twitterHandle: '@DocMaboos',
+        imagePath: 'assets/profiles/eaabfc300beb14a1257d6ba9326bb09a.png'
       },
       {
         name: 'Pascal Bayer',
-        twitter: '@PascalBayer',
-        image: 'assets/profiles/c5b8e259d9332bc0a9f3e7f4e936245a.png'
+        twitterHandle: '@PascalBayer',
+        imagePath: 'assets/profiles/c5b8e259d9332bc0a9f3e7f4e936245a.png'
       },
       {
         name: 'Alexander Schuster',
-        twitter: '@heyitsalex86',
-        image: 'assets/profiles/MVIMG_20180618_162813.jpg'
+        twitterHandle: '@heyitsalex86',
+        imagePath: 'assets/profiles/MVIMG_20180618_162813.jpg'
       }
     ]);
   }
-
-  ngOnInit() {}
 }
