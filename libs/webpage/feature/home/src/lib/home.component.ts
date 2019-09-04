afd6ba2c61bf9932ca8194cef9581fce27cea9f8
Alexander Schuster <alexander.schuster@web.de>
2019-09-04 07:50:38 +0200

    fix date error

--- libs/webpage/feature/home/src/lib/home.component.ts
+++ libs/webpage/feature/home/src/lib/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.nextMeetup$ = of({
-      date: new Date(2019, 6, 18).toJSON(),
+      date: new Date(2019, 8, 5).toJSON(),
       location: 'SPIRIT/21',
       meetupUrl:
         'https://www.meetup.com/de-DE/angular-users-stuttgart/events/263949770/'
